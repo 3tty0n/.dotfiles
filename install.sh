8bce99e6dffda3d130e9fca458a0b11fc74d8e38
YuIzawa <yuizalp@gmail.com>
2016-11-07 13:59:57 +0900

    add install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+ln -s ~/.dotfiles/.vimrc ~/.vimrc
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/.gitignore_global ~/.gitignore_global
