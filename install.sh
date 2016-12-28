b6ebcd3e777c12dc5cf620978c241c850dececef
Yusuke Izawa <yuizalp@gmail.com>
2016-12-29 03:10:26 +0900

    Add install process of homebrew

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew bundle
+
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
