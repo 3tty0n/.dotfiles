7fee33d31fb0dc3327da98df63f509df659ae30b
Yusuke Izawa <yuizalp@gmail.com>
2016-12-29 04:06:42 +0900

    Fix intall.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew tap Homebrew/bundle
 brew bundle
-
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
