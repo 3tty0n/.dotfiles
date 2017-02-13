afbcdf56f4b1966b97faad610ae28dd8cef3eaec
Yusuke Izawa <yuizalp@gmail.com>
2017-02-13 19:11:36 +0900

    fix brew settings

--- install-mini.sh
+++ install-mini.sh
@@ -1,8 +1,10 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+cd brew
 mv Brewfile _Brewfile
-mv miniBrewfile Brewfile
+mv Brewfile-mini Brewfile
 brew tap Homebrew/bundle
 brew bundle
+cd ../
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
