ade51af793bdb55d40b4c22412a77bd15824ee60
Yusuke Izawa <yuizalp@gmail.com>
2017-02-13 19:11:44 +0900

    add .irbrc and fix install scripts

--- install-mini.sh
+++ install-mini.sh
@@ -10,3 +10,4 @@ ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/.gitignore_global ~/.gitignore_global
+ln -s ~/.dotfiles/.irbrc ~/.irbrc
--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+cd brew
 brew tap Homebrew/bundle
 brew bundle
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
@@ -6,3 +7,4 @@ ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/.gitignore_global ~/.gitignore_global
+ln -s ~/.dotfiles/.irbrc ~/.irbrc
