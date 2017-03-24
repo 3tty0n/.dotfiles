b643334dd99fc2bf02b83d4a4716608cbd62971c
Yusuke Izawa <yuizalp@gmail.com>
2017-03-24 17:16:34 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,16 @@
-make build
-make oh-my-zsh
-make symlink
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+cp ./brew/Brewfile .
+brew tap Homebrew/bundle
+brew bundle
+rm Brewfile
+
+ln -s -i ~/.dotfiles/.vimrc ~/.vimrc
+ln -s -i ~/.dotfiles/.zshrc ~/.zshrc
+ln -s -i ~/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -s -i ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -s -i ~/.dotfiles/.gitignore_global ~/.gitignore_global
+ln -s- i ~/.dotfiles/.irbrc ~/.irbrc
+ln -s -i ~/.dotfies/vim/ftplugin ~/.vim/ftplugin
+ln -s -i ~/.dotfiles/vim/snippets ~/.vim/snippets
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
