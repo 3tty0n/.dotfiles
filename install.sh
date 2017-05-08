bf8005e4f96ebc3e02f6b9e15b41ff86b6053d73
Yusuke Izawa <yuizalp@gmail.com>
2017-05-08 21:25:23 +0900

    update install scripts

--- install.sh
+++ install.sh
@@ -4,16 +4,16 @@ brew tap Homebrew/bundle
 brew bundle
 rm Brewfile
 
-ln -s -i ~/.dotfiles/.vimrc ~/.vimrc
-ln -s -i ~/.dotfiles/.zshrc ~/.zshrc
-ln -s -i ~/.dotfiles/.tmux.conf ~/.tmux.conf
-ln -s -i ~/.dotfiles/.gitconfig ~/.gitconfig
-ln -s -i ~/.dotfiles/.gitignore_global ~/.gitignore_global
-ln -s- i ~/.dotfiles/.irbrc ~/.irbrc
-ln -s -i ~/.dotfies/vim/ftplugin ~/.vim/ftplugin
-ln -s -i ~/.dotfiles/vim/snippets ~/.vim/snippets
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+function mk_symlink() {
+		ln -s -i ~/.dotfiles/.vimrc ~/.vimrc
+		ln -s -i ~/.dotfiles/.zshrc ~/.zshrc
+		ln -s -i ~/.dotfiles/.tmux.conf ~/.tmux.conf
+		ln -s -i ~/.dotfiles/.gitconfig ~/.gitconfig
+		ln -s -i ~/.dotfiles/.gitignore_global ~/.gitignore_global
+		ln -s- i ~/.dotfiles/.irbrc ~/.irbrc
+		ln -s -i ~/.dotfies/vim/ftplugin ~/.vim/ftplugin
+		ln -s -i ~/.dotfiles/vim/snippets ~/.vim/snippets
+}
 
 git clone git@github.com:powerline/fonts.git
 ./fonts/install.sh
