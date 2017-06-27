7bb2fff6b65d48e03c35c5cc9ac713254ddd8648
Yusuke Izawa <yuizalp@gmail.com>
2017-06-28 00:54:40 +0900

    update .zplugrc.zsh

--- .zplugrc.zsh
+++ .zplugrc.zsh
@@ -1,8 +1,9 @@
 zplug "zsh-users/zsh-completions"
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 zplug "zsh-users/zsh-history-substring-search"
+zplug "zsh-users/zsh-autosuggestions"
 zplug "mollifier/cd-gitroot"
-zplug "b4b4r07/enhancd", use:enhancd.sh
+zplug "b4b4r07/enhancd", use:init.sh
 zplug "junegunn/fzf-bin", as:command, from:gh-r, rename-to:fzf
 zplug "peco/peco", as:command, from:gh-r, use:"*amd64*"
 
