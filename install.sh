782248713dc526d54bc43648de7f7e642609f10a
Yusuke Izawa <yuizalp@gmail.com>
2017-07-04 03:44:52 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ function install() {
   fi
 
   declare -a dotfiles=()
-  declare -a dotfiles=(".vimrc" ".tmux.conf" ".gitconfig" ".gitignore_global" ".irbrc" ".latexmkrc" ".gemrc" ".zsh")
+  declare -a dotfiles=(".vimrc" ".tmux.conf" ".gitconfig" ".gitignore_global" ".irbrc" ".latexmkrc" ".gemrc")
 
   declare -a vimdir=()
   declare -a vimdir=("ftplugin" "snippets")
@@ -24,12 +24,20 @@ function install() {
     ln -si ~/.dotfiles/${f} ~/${f}
   done
 
+  if [ ! -e ~/.zsh ]; then
+    ln -si ~/.dotfiles/.zsh ~/.zsh
+  fi
+
   for v in ${vimdir[@]}; do
     if [ ! -e ~/.vim ]; then
       mkdir ~/.vim
     fi
     ln -si ~/.dotfiles/.vim/${v} ~/.vim/${v}
   done
+
+  if [ ! -e ~/.zprezto ]; then
+    git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+  fi
 
   if [ ! -e ~/.zplug ]; then
     curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh| zsh
