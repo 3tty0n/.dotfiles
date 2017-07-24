7d31880225e6a758290a6084848f72677b9716d3
Yusuke Izawa <yuizalp@gmail.com>
2017-07-24 09:05:52 +0900

    move fzf settings to my gist and add some fixes

--- .zsh/.zplugrc.zsh
+++ .zsh/.zplugrc.zsh
@@ -13,21 +13,23 @@ zplug "b4b4r07/emoji-cli", \
     on:"stedolan/jq"
 zplug "b4b4r07/zsh-gomi", if:"which fzf"
 zplug "changyuheng/zsh-interactive-cd"
-zplug "tsub/4448666a276b088bce3f19005f512c15", from:gist # ghq-fzf.zsh
 zplug "tsub/f4036e067a59b242a161fc3c8a5f01dd", from:gist # history-fzf.zsh
 zplug "tsub/81ac9b881cf2475977c9cb619021ef3c", from:gist # ssh-fzf.zsh
 zplug "tsub/29bebc4e1e82ad76504b1287b4afba7c", from:gist # tree-fzf.zsh
+zplug "3tty0n/259e8d65b0e7fd375d6c9545d67e85f0", from:gist # fzf-git-log.zsh
+zplug "3tty0n/fbfa5b88feed5bc5d3b5f5e59d80a2d8", from:gist # fzf-git-branch.zsh
+zplug "3tty0n/b5a035e611357e1805b690c7f0e4b1c2", from:gist # fzf-git-add.zsh
+zplug "3tty0n/1a5ed257ca3cdda411386c2cbd3b1acc", from:gist # fzf-git-stash.zsh
+zplug "3tty0n/0c7287deedd0bedc1bc0dc23b51653aa", from:gist # ghq-fzf.zsh
 
 # oh-my-zsh
 zplug "plugins/extract", from:oh-my-zsh
 
-# Install plugins if there are plugins that have not been installed
 if ! zplug check --verbose; then
-    printf "Install? [y/N]: "
-    if read -q; then
-        echo; zplug install
-    fi
+  printf "Install? [y/N]: "
+  if read -q; then
+      echo; zplug install
+  fi
 fi
 
-# Then, source plugins and add commands to $PATH
 zplug load --verbose
