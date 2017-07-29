6c993b9e16454144fae0d999a005fdec91e51a47
Yusuke Izawa <yuizalp@gmail.com>
2017-07-29 18:55:37 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,8 @@ function install_brew_packages() {
 }
 
 function install() {
+  cd ~/.dotfiles/
+
   if [ ! -e /usr/local/Cellar ]; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   fi
@@ -20,18 +22,18 @@ function install() {
   declare -a dotfiles=(".vimrc" ".tmux.conf" ".gitconfig" ".gitignore_global" ".irbrc" ".latexmkrc" ".gemrc" ".zshrc" ".vimrc.local.vim")
 
   for f in ${dotfiles[@]}; do
-    ln -si ~/.dotfiles/${f} ~/${f}
+    ln -si `pwd`/${f} ~/${f}
   done
 
   if [ ! -e ~/.zsh ]; then
-    ln -si ~/.dotfiles/.zsh ~/.zsh
+    ln -si `pwd`/.zsh ~/.zsh
   fi
 
   if [ ! -e ~/.vim ]; then
     mkdir ~/.vim
   fi
-  ln -si ~/.dotfiles/.vim/ftplugin ~/.vim/ftplugin
-  ln -si ~/.dotfiles/.vim/snippets ~/.vim/snippets
+  ln -si `pwd`/.vim/ftplugin ~/.vim/ftplugin
+  ln -si `pwd`/.vim/snippets ~/.vim/snippets
 
   if [ ! -e ~/.zprezto ]; then
     git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
