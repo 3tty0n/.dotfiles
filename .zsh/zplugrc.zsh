1475fa085a889afb5792a26b31d0a9541e2dd974
Yusuke Izawa <yuizalp@gmail.com>
2017-08-01 18:31:57 +0900

    use prezot module to speed up

--- .zsh/zplugrc.zsh
+++ .zsh/zplugrc.zsh
@@ -1,5 +1,5 @@
-zplug "zsh-users/zsh-history-substring-search"
-zplug "zsh-users/zsh-autosuggestions"
+#zplug "zsh-users/zsh-history-substring-search"
+#zplug "zsh-users/zsh-autosuggestions"
 zplug "b4b4r07/enhancd", use:init.sh
 zplug "junegunn/fzf-bin", as:command, from:gh-r, rename-to:fzf
 zplug "peco/peco", as:command, from:gh-r, use:"*amd64*"
