1a9879caeecbe9d9449a711fba0b6cac29f87360
Yusuke Izawa <yuizalp@gmail.com>
2017-08-02 02:10:01 +0900

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -17,8 +17,10 @@ done
 [ ! -e ~/.config/fish ] && mkdir -p ~/.config/fish
 ln -sfnv $DOTFILES_ROOT/.config/fish/config.fish ~/.config/fish
 
-[ ! -e ~/.config/gist ] && mkdir -p ~/.config/gist
-# ln -sfnv $DOTFILES_ROOT/.config/gist/config.toml ~/.config/gist
+if [ ! -e ~/.config/gist ]; then
+  mkdir -p ~/.config/gist
+  ln -sfnv $DOTFILES_ROOT/.config/gist/config.toml ~/.config/gist
+fi
 
 if [ ! -e ~/.zprezto ]; then
   git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
