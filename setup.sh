2b835708489850f66ad5bcb740429bb9dbbefd16
Yusuke Izawa <yuizalp@gmail.com>
2017-08-07 06:53:36 +0900

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -30,7 +30,3 @@ fi
 if [ ! -e ~/.zprezto ]; then
   git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 fi
-
-#if [ ! -e ~/.zplug ]; then
-#  curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh| zsh
-#fi
