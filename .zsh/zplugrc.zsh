30d2cdfe90ef29b8ead268eadcdf5d08a6f76598
Yusuke Izawa <yuizalp@gmail.com>
2017-08-10 08:41:20 +0900

    update zplugrc.zsh

--- .zsh/zplugrc.zsh
+++ .zsh/zplugrc.zsh
@@ -1,12 +1,28 @@
+zplug "zplug/zplug", hook-build:'zplug --self-manage'
+
 zplug "zsh-users/zsh-completions"
-zplug "zsh-users/zsh-history-substring-search"
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
+zplug "zsh-users/zsh-history-substring-search"
 zplug "zsh-users/zsh-autosuggestions"
-zplug "b4b4r07/enhancd", use:init.sh
 zplug "changyuheng/zsh-interactive-cd"
-zplug "clvv/fasd", as:command, use:fasd
 zplug "mollifier/cd-gitroot"
 zplug "~/.zsh/functions", from:local
+
+zplug "clvv/fasd", \
+      as:command, \
+      use:fasd
+
+zplug "b4b4r07/enhancd", \
+      use:init.sh
+
+zplug "~/.zsh/util", \
+      from:local
+
+zplug "motemen/ghq", \
+    as:command, \
+    from:gh-r, \
+    rename-to:ghq
+
 
 # gist
 zplug "tsub/f4036e067a59b242a161fc3c8a5f01dd", from:gist # history-fzf.zsh
