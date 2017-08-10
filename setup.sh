2e1b6ce0752f71f1050b46c82ed63dc6e323af2b
Yusuke Izawa <yuizalp@gmail.com>
2017-08-10 09:00:58 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,6 +11,9 @@ declare -a vimfiles=(ftplugin snippets)
 declare -a configfiles=()
 declare -a configfiles=(fish omf)
 
+declare -a ghqrepos=()
+declare -a ghqrepos=(banga/powerline-shell bahlo/iterm-colors t3chnoboy/thayer-bright-iTerm)
+
 for f in ${dotfiles[@]}; do
   ln -sfnv $DOTFILES_ROOT/${f} ~/${f}
 done
@@ -27,22 +30,11 @@ if [ ! -e ~/.config/gist ]; then
   cp -v $DOTFILES_ROOT/.config/gist/config.toml ~/.config/gist
 fi
 
-if [ ! -e ~/.zprezto ]; then
-  git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-fi
-
 if [ ! -e ~/.zplug ]; then
   curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh| zsh
 fi
-if [ ! -x "`which ghq`" ]; then
-  if [ ! -x "`which go`" ]; then
-    echo "Please install go."
-    exit 1
-  fi
-  go get github.com/motemen/ghq
-fi
 
-for repo in banga/powerline-shell bahlo/iterm-colors t3chnoboy/thayer-bright-iTerm; do
+for repo in ${ghqrepos[@]}; do
   if [ ! -d `ghq root`/github.com/$repo ]; then
     ghq get https://github.com/$repo
   fi
