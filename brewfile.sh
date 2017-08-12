782ca6921d28bf07ae23d79cd6c1d99ff74e0fc4
Yusuke Izawa <yuizalp@gmail.com>
2017-08-12 20:11:12 +0900

    fix brewfile.sh

--- brewfile.sh
+++ brewfile.sh
@@ -3,7 +3,7 @@
 if [ ! -x "`which brew`" ]; then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
-cp ./brew/Brewfile .
+
+cd brew
 brew tap Homebrew/bundle
 brew bundle
-rm -f ./Brewfile
