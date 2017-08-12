73e36e9262587d478e357478bcc8d36ea6edbae2
Yusuke Izawa <yuizalp@gmail.com>
2017-08-12 15:35:20 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 set -eu
 
 is_master=false
@@ -18,18 +18,28 @@ usage () {
   exit 1
 }
 
-exec_script () {
-  ./setup.sh
-  ./brewfile.sh
+spin (){
+  pid=$! # Process Id of the previous running command
+
+  spin='-\|/'
+
+  i=0
+  while kill -0 $pid 2>/dev/null
+  do
+    i=$(( (i+1) %4 ))
+    printf "\r${spin:$i:1}"
+    sleep .1
+  done
 }
 
 install () {
-  git clone git@github.com:3tty0n/.dotfiles.git ~/.dotfiles && cd ~/.dotfiles
-  git checkout $BRANCH
-  exec_script
+  if [ ! -d ~/.dotfiles ];then
+    git clone git@github.com:3tty0n/.dotfiles.git ~/.dotfiles &/dev/null
+  fi
+  git checkout $BRANCH &>/dev/null
+  ./setup.sh &>/dev/null
+  ./brewfile.sh &>/dev/null
 }
-
-# [[ -d ${HOME}/.dotfiles ]] && mv ${HOME}/.dotfiles ${HOME}/.dotfiles.orig
 
 for OPT in "$@"; do
   case "$OPT" in
@@ -53,7 +63,7 @@ for OPT in "$@"; do
     -*)
       echo "`basename $0`: illegal option -- '$(echo $1 | sed 's/^-*//')'" 1>&2
       exit 1
-      ;;    
+      ;;
     *)
       if [[ ! -z "$1" ]] && [[ ! "$1" =~ ^-+ ]]; then
         #param=( ${param[@]} "$1" )
@@ -67,9 +77,9 @@ done
 if [ -z "$BRANCH" ]; then
   usage
 else
-  echo "Install in $BRANCH branch ..."
-  echo
-  install
-  echo "installation is finished."
-  echo
+  printf " Install in $BRANCH branch ...\n"
+  install & spin && printf "\n"
+  printf " All processes are successfully completed \U1F389\n"
+  printf " For more information, see ${(%):-%U}https://github.com/3tty0n/.dotfiles${(%):-%u} \U1F33A\n"
+  printf " Enjoy hacking!\n"
 fi
