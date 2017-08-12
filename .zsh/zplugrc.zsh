fcc56ce20e413903a89ccf6f2db2905a4b9136b5
Yusuke Izawa <yuizalp@gmail.com>
2017-08-12 16:43:38 +0900

    fix zplugrc.zsh

--- .zsh/zplugrc.zsh
+++ .zsh/zplugrc.zsh
@@ -6,7 +6,6 @@ zplug "zsh-users/zsh-history-substring-search"
 zplug "zsh-users/zsh-autosuggestions"
 zplug "changyuheng/zsh-interactive-cd"
 zplug "mollifier/cd-gitroot"
-zplug "~/.zsh/util", from:local
 
 zplug "rupa/z", use:z.sh
 
@@ -14,9 +13,9 @@ zplug "b4b4r07/enhancd", \
       use:init.sh
 
 zplug "motemen/ghq", \
-    as:command, \
-    from:gh-r, \
-    rename-to:ghq
+      as:command, \
+      from:gh-r, \
+      rename-to:ghq
 
 zplug "junegunn/fzf-bin", \
       from:gh-r, \
@@ -26,5 +25,7 @@ zplug "junegunn/fzf-bin", \
 zplug "peco/peco", \
       as:command, \
       from:gh-r
+
+zplug "~/.zsh/util", from:local
 
 zplug load
