332a0b548c97637c0d194155d8a94139fd1b33b7
Yusuke Izawa <yuizalp@gmail.com>
2017-08-28 20:12:32 +0900

    update

--- .zsh/util/fzf-functions.zsh
+++ .zsh/util/fzf-functions.zsh
@@ -84,3 +84,17 @@ __gol_fzf () {
 
 zle -N __gol_fzf
 bindkey '^]' __gol_fzf
+
+fco() {
+  local tags branches target
+  tags=$(
+    git tag | awk '{print "\x1b[31;1mtag\x1b[m\t" $1}') || return
+  branches=$(
+    git branch --all | grep -v HEAD             |
+    sed "s/.* //"    | sed "s#remotes/[^/]*/##" |
+    sort -u          | awk '{print "\x1b[34;1mbranch\x1b[m\t" $1}') || return
+  target=$(
+    (echo "$tags"; echo "$branches") |
+    fzf-tmux -l30 -- --no-hscroll --ansi +m -d "\t" -n 2) || return
+  git checkout $(echo "$target" | awk '{print $2}')
+}
