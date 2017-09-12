c06367b23ae176a058ba6344a42ed6d004589db6
Yusuke Izawa <yuizalp@gmail.com>
2017-09-12 21:18:25 +0900

    update

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ usage () {
   exit 1
 }
 
-spin (){
+spinner() {
   pid=$! # Process Id of the previous running command
 
   spin='-\|/'
@@ -28,49 +28,6 @@ spin (){
     printf "\r${spin:$i:1}"
     sleep .1
   done
-}
-
-move() {
-  col="$1"
-  line="$2"
-  printf "\r\033[${line};${col}f"
-}
-
-eraceCurrentLine() {
-  printf "\033[2K\r"
-}
-
-get_line() {
-  echo -ne "\r\033[6n"
-  read -s -d\[ garbage
-  read -s -d R foo
-  REPLY=$(echo "$foo" | sed 's/;.*$//')
-}
-
-sp() {
-  local    spinner
-  local -a spinners
-  spinners=(⠋ ⠙ ⠹ ⠸ ⠼ ⠴ ⠦ ⠧ ⠇ ⠏)
-
-  #line
-  #line=$REPLY
-  line=$1
-
-  sleep ${3:-1} & id=$!
-
-  while [[ $jobstates =~ $id ]]
-  do
-    for spinner in "${spinners[@]}"
-    do
-      sleep 0.05
-      printf "\r\033[$line;1f $fg[white]$spinner$reset_color  Installing...  $2" 2>/dev/null
-      [[ $jobstates =~ $id ]] || break
-    done
-  done
-
-  move 1 $line
-  eraceCurrentLine
-  printf " $fg_bold[blue]\U2714$reset_color  $fg[green]Installed!$reset_color     $2\n"
 }
 
 install () {
@@ -117,16 +74,8 @@ for OPT in "$@"; do
 done
 
 (
-  printf "\n\n"
-  # hide cursor
-  tput civis
-
-  get_line
-  line=$REPLY
-  install & { sp $(( line+=0 )) "${BRANCH}"}
-
+  install & spinner
   # show cursor
-  tput cnorm
   printf "\n\n"
   printf " All processes are successfully completed \U1F389\n"
   printf " For more information, see ${(%):-%U}https://github.com/3tty0n/.dotfiles${(%):-%u} \U1F33A\n"
