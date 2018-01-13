8d28dd9c4453199f7b92867aacf95d51843215ee
Yusuke Izawa <yuizalp@gmail.com>
2018-01-13 18:27:13 +0900

    update [Sat Jan 13 18:27:13 JST 2018]

--- .zsh/util/history-fzf.zsh
+++ .zsh/util/history-fzf.zsh
@@ -1,10 +1,8 @@
-__fzf-select-history() {
-  BUFFER=$(history 1 |
-      sort -k1,1nr |
-      perl -ne 'BEGIN { my @lines = (); } s/^\s*\d+\*?\s*//; $in=$_; if (!(grep {$in eq $_} @lines)) { push(@lines, $in); print $in; }' |
-      fzf --query "$LBUFFER")
-  CURSOR=${#BUFFER}
+function __fzf-select-history() {
+  BUFFER=$(history -n 1 | tail -r  | awk '!a[$0]++' | fzf --no-sort +m --prompt="History > ")
+  CURSOR=$#BUFFER
   zle reset-prompt
 }
+
 zle -N __fzf-select-history
 bindkey '^r' __fzf-select-history
