358ff6573fc04717e903fedcfaf985b638fcf86d
Yusuke Izawa <yuizalp@gmail.com>
2018-06-02 14:06:42 +0900

    update

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -118,6 +118,28 @@
 ;; yasnippet
 (yas-global-mode 1)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;; git settings ;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; git-gutter
+(global-git-gutter-mode +1)
+
+;; Magit
+(setq-default magit-auto-revert-mode nil)
+(setq vc-handled-backends '())
+(eval-after-load "vc" '(remove-hook 'find-file-hooks 'vc-find-file-hook))
+(global-set-key (kbd "C-c m") 'magit-status)
+
+(custom-set-faces
+ '(magit-diff-added ((t (:background "black" :foreground "green"))))
+ '(magit-diff-added-highlight ((t (:background "white" :foreground "green"))))
+ '(magit-diff-removed ((t (:background "black" :foreground "blue"))))
+ '(magit-diff-removed-hightlight ((t (:background "white" :foreground "blue"))))
+ '(magit-hash ((t (:foreground "red"))))
+)
+
+
 ;;;;; language
 
 ;; ocaml
