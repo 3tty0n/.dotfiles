a7f103cda632bcddc972f4bb454eefda55acc560
Yusuke Izawa <yuizalp@gmail.com>
2018-06-04 06:44:37 +0900

    update .emacs.d

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -51,6 +51,44 @@
   (if (bolp) (back-to-indentation) (beginning-of-line)))
 (define-key global-map "\C-a" 'back-to-indentation-or-beginning)
 
+;; window
+(defun window-resizer ()
+  "Control window size and position."
+  (interactive)
+  (let ((window-obj (selected-window))
+        (current-width (window-width))
+        (current-height (window-height))
+        (dx (if (= (nth 0 (window-edges)) 0) 1
+              -1))
+        (dy (if (= (nth 1 (window-edges)) 0) 1
+              -1))
+        c)
+    (catch 'end-flag
+      (while t
+        (message "size[%dx%d]"
+                 (window-width) (window-height))
+        (setq c (read-char))
+        (cond ((= c ?l)
+               (enlarge-window-horizontally dx))
+              ((= c ?h)
+               (shrink-window-horizontally dx))
+              ((= c ?j)
+               (enlarge-window dy))
+              ((= c ?k)
+               (shrink-window dy))
+              ;; otherwise
+              (t
+               (message "Quit")
+               (throw 'end-flag t)))))))
+
+(define-key global-map "\C-q" (make-sparse-keymap)) ;; C-q をプリフィックスキー化
+(global-set-key "\C-q\C-q" 'quoted-insert) ;; quoted-insert は C-q C-q へ割り当て
+(global-set-key "\C-q\C-r" 'window-resizer) ;; window-resizer は C-q C-r (resize)
+(global-set-key "\C-ql" 'windmove-right)
+(global-set-key "\C-qh" 'windmove-left)
+(global-set-key "\C-qj" 'windmove-down)
+(global-set-key "\C-qk" 'windmove-up)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;; color theme ;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
