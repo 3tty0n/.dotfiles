64d060a5f5c86648a258e300b543abb4cca4a852
Yusuke Izawa <yuizalp@gmail.com>
2018-09-12 00:45:31 +0900

    [emacs] update

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -51,6 +51,9 @@
   (if (bolp) (back-to-indentation) (beginning-of-line)))
 (define-key global-map "\C-a" 'back-to-indentation-or-beginning)
 
+;; set C-h to backspace
+(global-set-key (kbd "C-h") 'delete-backward-char)
+
 ;; for window system
 (if window-system
     (progn
@@ -399,6 +402,31 @@
 
 ;; markdown
 (setq markdown-command "multimarkdown") ; require multimarkdown command `brew install multimarkdown'
+
+;; org-mode
+(with-eval-after-load 'org-mode
+  (setq org-startup-with-inline-images t)
+  ;; 見出しの余分な*を消す
+  (setq org-hide-leading-stars t)
+  ;; LOGBOOK drawerに時間を格納する
+  (setq org-clock-into-drawer t)
+  ;; .orgファイルは自動的にorg-mode
+  (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+  ;; org-directory内のファイルすべてからagendaを作成する
+  (setq my-org-agenda-dir "~/org/")
+  (setq org-agenda-files (list my-org-agenda-dir))
+  ;; TODO状態
+  (setq org-todo-keywords
+	'((sequence "TODO(t)" "WAIT(w)" "NOTE(n)"  "|" "DONE(d)" "SOMEDAY(s)" "CANCEL(c)")))
+  ;; DONEの時刻を記録
+  (setq org-log-done 'time)
+  ;; keybindings
+  (global-set-key "\C-cl" 'org-store-link)
+  (global-set-key "\C-cc" 'org-capture)
+  (global-set-key "\C-ca" 'org-agenda)
+  (global-set-key "\C-cb" 'org-iswitchb)
+  )
+
 ;; ## added by OPAM user-setup for emacs / base ## 56ab50dc8996d2bb95e7856a6eddb17b ## you can edit, but keep this line
 ;; ## end of OPAM user-setup addition for emacs / base ## keep this line
 (custom-set-variables
