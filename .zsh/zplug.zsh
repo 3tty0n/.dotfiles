f04a29ebcd11ea86376896f88e0c0ed6bd44c5bd
Yusuke Izawa <yuizalp@gmail.com>
2018-10-22 00:50:56 +0900

    update [Mon Oct 22 00:50:56 JST 2018]

--- .zsh/zplug.zsh
+++ .zsh/zplug.zsh
@@ -2,7 +2,8 @@ zplug "zplug/zplug", hook-build:'zplug --self-manage'
 
 zplug "zsh-users/zsh-completions"
 
-zplug "zsh-users/zsh-syntax-highlighting", defer:2
+# zplug "zsh-users/zsh-syntax-highlighting", defer:2
+zplug "zdharma/fast-syntax-highlighting", defer:2
 
 zplug "zsh-users/zsh-history-substring-search"
 
