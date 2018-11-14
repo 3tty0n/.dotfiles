dea39ef3ad8f104d3f80fdecdc259825f3da5331
Yusuke Izawa <yuizalp@gmail.com>
2018-11-14 23:12:59 +0900

    [emacs] update

--- .emacs.d/install.sh
+++ .emacs.d/install.sh
@@ -21,10 +21,31 @@ cask install
 
 echo "Done."
 
-echo "Installing OCaml environment..."
+echo "setting up for ricty font..."
 
-if [ ! -d $HOME/.opam ]; then
-    case "${OSTYPE}" in
+if [ ! -x "$(which brew)"]; then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+brew tap sanemat/font
+brew install Caskroom/cask/xquartz
+brew install ricty --with-powerline
+
+cp -f /usr/local/opt/ricty/share/fonts/Ricty*.ttf ~/Library/Fonts/
+fc-cache -vf
+
+echo "Done."
+
+echo "setting up for multi-term..."
+
+curl https://opensource.apple.com/source/emacs/emacs-70/emacs/etc/e/eterm-color.ti\?txt > eterm-color.ti 2>/dev/null
+tic -o ~/.terminfo eterm-color.ti 2>/dev/null
+
+echo "Done."
+
+echo "installing ocaml environment..."
+
+if [ ! -d $home/.opam ]; then
+    case "${ostype}" in
         darwin* )
             brew install opam
             ;;
@@ -36,11 +57,4 @@ fi
 
 opam install -y ${deps[@]}
 
-echo "Done"
-
-echo "Setting up for multi-term..."
-
-curl https://opensource.apple.com/source/emacs/emacs-70/emacs/etc/e/eterm-color.ti\?txt > eterm-color.ti 2>/dev/null
-tic -o ~/.terminfo eterm-color.ti 2>/dev/null
-
-echo "Done."
+echo "done"
