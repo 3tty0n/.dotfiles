9db09a858b99b4e05ed92529dc608e76013761f6
Yusuke Izawa <yuizalp@gmail.com>
2018-12-19 15:11:08 +0900

    remove history-peco

--- .zsh/util/history-peco.zsh
+++
@@ -1,7 +0,0 @@
-function __select-history () {
-    BUFFER=$(history -n 1 | tail -r | awk '!a[$0]++' | peco)
-    CURSOR=$#BUFFER
-    zle reset-prompt
-}
-zle -N __select-history
-bindkey '^R' __select-history
