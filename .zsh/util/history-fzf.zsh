a52b77dfbe87d4de70caa4524aa078172cffb556
Yusuke Izawa <yuizalp@gmail.com>
2018-12-31 15:35:42 +0900

    remove history-fzf

--- .zsh/util/history-fzf.zsh
+++
@@ -1,8 +0,0 @@
-function __fzf-select-history() {
-  BUFFER=$(history -n 1 | tail -r  | awk '!a[$0]++' | fzf)
-  CURSOR=$#BUFFER
-  zle reset-prompt
-}
-
-zle -N __fzf-select-history
-bindkey '^r' __fzf-select-history
