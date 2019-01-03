009f68104d77b4d2c58ff2cc6b3fde16f89151ee
Yusuke Izawa <yuizalp@gmail.com>
2019-01-03 20:02:03 +0900

    fix install_ricty.sh

--- fonts/install_ricty.sh
+++ fonts/install_ricty.sh
@@ -4,7 +4,7 @@ case "${OSTYPE}" in
   darwin* )
     brew tap sanemat/font
     brew install ricty
-    cp -f /usr/local/Cellar/ricty/4.0.1/share/fonts/Ricty*.ttf ~/Library/Fonts/
+    cp -f /usr/local/share/fonts/Ricty*.ttf ~/Library/Fonts
     fc-cache -vf
     ;;
 esac
