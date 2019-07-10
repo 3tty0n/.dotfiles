704e330cba3b7e84461e59a131d991ed1fa80a47
Yusuke Izawa <yuizalp@gmail.com>
2019-07-10 07:52:24 -0700

    update

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -6,7 +6,7 @@ myWorkSpaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 myTerminal = "gnome-terminal"
 
 main = xmonad defaultConfig
-  { modMask = mod1Mask -- Use left Alt instead of Alt
+  { modMask = mod4Mask -- Use left Alt instead of Alt
   , terminal = myTerminal
   , workspaces = myWorkSpaces 
   }
