5648d238c6ae90bcde532cb9638acbbd8bed843b
Yusuke Izawa <yuizalp@gmail.com>
2019-07-24 17:42:44 +0900

    update xmobar and xmonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,17 +1,36 @@
 -- minimal Ubuntu config file: ~/.xmonad/xmonad.hs
 import XMonad
+import XMonad.Layout
+import XMonad.Layout.Circle
+import XMonad.Layout.Column
+import XMonad.Layout.ThreeColumns
+import XMonad.Layout.MultiColumns
+import XMonad.Layout.Magnifier
+import XMonad.Layout.NoBorders
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
+-- Layout management
+-- defaultLayoutHook = avoidStruts $ layoutHook defaultConfig 
+myLayoutHook =
+  avoidStruts $ (tall ||| threeCol ||| Full)
+  where
+    master = 1
+    ratioInc = (3/100)
+    ratio = (1/2)
+
+    tall = Tall master ratioInc ratio
+    threeCol = ThreeColMid master ratioInc ratio
+
 main = do
     xmproc <- spawnPipe "xmobar"
 
     xmonad $ defaultConfig
         { manageHook = manageDocks <+> manageHook defaultConfig
-        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , layoutHook = myLayoutHook
         , handleEventHook = handleEventHook defaultConfig <+> docksEventHook
         , logHook = dynamicLogWithPP xmobarPP
                         { ppOutput = hPutStrLn xmproc
