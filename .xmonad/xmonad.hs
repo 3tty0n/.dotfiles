bcbf4d36cbbd41bcc90ff458ec91c63391dd05ff
Yusuke Izawa <yuizalp@gmail.com>
2019-07-27 21:23:07 +0900

    update [Sat Jul 27 21:23:07 JST 2019]

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -42,7 +42,7 @@ main = do
       , modMask = myMod
       , terminal = myTerminal
       } `additionalKeys`
-      [ ((mod4Mask .|. shiftMask, xK_z), spawn "blackout")
+      [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock; xset dpms force off")
       , ((mod4Mask .|. shiftMask, xK_f), spawn "firefox")
       , ((mod4Mask .|. shiftMask, xK_e), spawn "emacs")
       , ((controlMask, xK_Print       ), spawn "sleep 0.2; scrot -s")
