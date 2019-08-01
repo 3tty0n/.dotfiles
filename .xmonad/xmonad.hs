614e768e73c7f3ad315361e2f040fe4820beab8b
Yusuke Izawa <yuizalp@gmail.com>
2019-08-02 05:03:39 +0900

    update [Fri Aug  2 05:03:39 JST 2019]

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -8,6 +8,7 @@ import XMonad.Layout.Gaps
 import XMonad.Layout.Grid
 import XMonad.Layout.Spacing
 import XMonad.Layout.ThreeColumns
+import XMonad.Layout.Magnifier
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
@@ -20,17 +21,19 @@ myTerminal = "gnome-terminal"
 
 -- Layout management
 myLayoutHook =
-  spacing 10 $ gaps [(L, 10), (R, 10)] $
-    avoidStruts $ (tall ||| Grid ||| Circle ||| threeCol ||| Full)
+  -- spacing 10 $ gaps [(L, 10), (R, 10)] $
+  avoidStruts $ magnifiercz magRatio $ (tall ||| Grid ||| threeCol ||| magCircle ||| Full)
   where
     master = 1
     ratioInc = (3/100)
     ratio = (1/2)
+    magRatio = 2
 
     tall = Tall master ratioInc ratio
     cTall = centerMaster tall
     threeCol = ThreeColMid master ratioInc ratio
 
+    magCircle = Circle
     cGrid = centerMaster Grid
 
 main = do
@@ -53,4 +56,9 @@ main = do
       , ((mod4Mask .|. shiftMask, xK_f), spawn "firefox")
       , ((mod4Mask .|. shiftMask, xK_e), spawn "emacs")
       , ((controlMask, xK_Print       ), spawn "sleep 0.2; scrot -s")
+      , ((mod4Mask .|. controlMask              , xK_equal ), sendMessage MagnifyMore)
+      , ((mod4Mask .|. controlMask              , xK_minus), sendMessage MagnifyLess)
+      , ((mod4Mask .|. controlMask              , xK_o    ), sendMessage ToggleOff  )
+      , ((mod4Mask .|. controlMask .|. shiftMask, xK_o    ), sendMessage ToggleOn   )
+      , ((mod4Mask .|. controlMask              , xK_m    ), sendMessage Toggle     )
       ]
