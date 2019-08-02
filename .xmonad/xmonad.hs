93077a74eaafa1282f3e90b74ad07eabea910040
Yusuke Izawa <yuizalp@gmail.com>
2019-08-02 13:35:29 +0900

    update [Fri Aug  2 13:35:29 JST 2019]

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -2,11 +2,13 @@
 import XMonad
 import XMonad.Layout
 import XMonad.Layout.Circle
+import XMonad.Layout.Cross
 import XMonad.Layout.Column
 import XMonad.Layout.CenteredMaster
 import XMonad.Layout.Gaps
 import XMonad.Layout.Grid
 import XMonad.Layout.Spacing
+import XMonad.Layout.Maximize
 import XMonad.Layout.ThreeColumns
 import XMonad.Layout.Magnifier
 import XMonad.Hooks.DynamicLog
@@ -15,26 +17,34 @@ import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig
 import System.IO
 
+import qualified Data.Map        as M
+import qualified XMonad.StackSet as W
+
 myMod = mod4Mask
 
 myTerminal = "gnome-terminal"
 
 -- Layout management
 myLayoutHook =
-  -- spacing 10 $ gaps [(L, 10), (R, 10)] $
+  spacing 10 $ gaps [(L, 10), (R, 10)] $
   avoidStruts $ magnifiercz magRatio $ (tall ||| Grid ||| threeCol ||| magCircle ||| Full)
   where
     master = 1
     ratioInc = (3/100)
     ratio = (1/2)
-    magRatio = 2
+    magRatio = 1.1
 
     tall = Tall master ratioInc ratio
-    cTall = centerMaster tall
     threeCol = ThreeColMid master ratioInc ratio
 
     magCircle = Circle
     cGrid = centerMaster Grid
+
+myMouseBindings (XConfig {XMonad.modMask = modMask}) = M.fromList $
+    [ ((modMask, button1), (\w -> focus w >> mouseMoveWindow w))
+    , ((modMask, button2), (\w -> focus w >> windows W.swapMaster))
+    , ((modMask, button3), (\w -> focus w >> mouseResizeWindow w))
+    ]
 
 main = do
     xmproc <- spawnPipe "xmobar"
@@ -49,12 +59,14 @@ main = do
                   }
       , modMask = myMod
       , terminal = myTerminal
+      , mouseBindings = myMouseBindings
       } `additionalKeys`
       [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock; xset dpms force off")
       , ((mod4Mask .|. shiftMask, xK_s), spawn "slock")
       , ((mod4Mask .|. shiftMask, xK_a), spawn "arandr")
       , ((mod4Mask .|. shiftMask, xK_f), spawn "firefox")
       , ((mod4Mask .|. shiftMask, xK_e), spawn "emacs")
+      , ((mod4Mask .|. shiftMask, xK_q), spawn "xscreensaver-command -lock; xset dpms force off")
       , ((controlMask, xK_Print       ), spawn "sleep 0.2; scrot -s")
       , ((mod4Mask .|. controlMask              , xK_equal ), sendMessage MagnifyMore)
       , ((mod4Mask .|. controlMask              , xK_minus), sendMessage MagnifyLess)
