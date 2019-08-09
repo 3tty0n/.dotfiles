be911c429859eebae80ef4f57de18f450604e17b
Yusuke Izawa <yuizalp@gmail.com>
2019-08-09 17:24:04 +0900

    update xmonad.hs

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,5 +1,6 @@
 -- minimal Ubuntu config file: ~/.xmonad/xmonad.hs
 import XMonad
+import XMonad.Actions.SinkAll
 import XMonad.Layout
 import XMonad.Layout.Circle
 import XMonad.Layout.Cross
@@ -28,7 +29,7 @@ myFocusedBorderColor = "#3399ff"
 
 -- Layout management
 myLayoutHook =
-  doSpacing $ avoidStruts $ (tall ||| Grid ||| threeCol ||| Full)
+  doSpacing $ avoidStruts $ (tall ||| Grid ||| threeCol ||| magnify Circle ||| Full)
   where
     master = 1
     ratioInc = (3/100)
@@ -44,21 +45,21 @@ myLayoutHook =
 
 myMouseBindings (XConfig {XMonad.modMask = modMask}) = M.fromList $
     [ ((modMask, button1), (\w -> focus w >> mouseMoveWindow w))
-    , ((modMask, button2), (\w -> focus w >> windows W.swapMaster))
+    , ((modMask, button2), (\w -> focus w >> windows W.shiftMaster))
     , ((modMask, button3), (\w -> focus w >> mouseResizeWindow w))
     ]
 
 myKeys =
-  [ ((mod4Mask, xK_p), spawn "rofi -show run")
-  , ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock; xset dpms force off")
+  [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock; xset dpms force off")
   , ((mod4Mask .|. shiftMask, xK_s), spawn "slock")
   , ((mod4Mask .|. shiftMask, xK_a), spawn "arandr")
   , ((mod4Mask .|. shiftMask, xK_e), spawn "emacs")
   , ((mod4Mask .|. shiftMask, xK_b), spawn "google-chrome-stable")
   , ((mod4Mask .|. shiftMask, xK_l), spawn "~/.screenlayout/laptop.sh")
-  , ((mod4Mask .|. shiftMask, xK_f), spawn "~/.screenlayout/laptop-full.sh")
-  , ((mod4Mask .|. shiftMask, xK_q), spawn "xscreensaver-command -lock; xset dpms force off")
+  , ((mod4Mask .|. shiftMask, xK_f), sinkAll)
+  -- , ((mod4Mask .|. shiftMask, xK_q), spawn "xscreensaver-command -lock; xset dpms force off")
   , ((controlMask, xK_Print       ), spawn "sleep 0.2; scrot -s")
+  , ((mod4Mask, xK_p), spawn "rofi -show run")
   , ((mod4Mask .|. controlMask              , xK_equal), sendMessage MagnifyMore)
   , ((mod4Mask .|. controlMask              , xK_minus), sendMessage MagnifyLess)
   , ((mod4Mask .|. controlMask              , xK_o    ), sendMessage ToggleOff  )
